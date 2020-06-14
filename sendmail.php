b3f15e161fc5128ba51d51d0f72743837fde8aba
Mehadi Hasan <bdmehadih@gmail.com>
2020-06-14 14:02:53 +0600

    first commit

---
+++ sendmail.php
@@ -0,0 +1,27 @@
+<?php 
+require 'phpmailer/PHPMailerAutoload.php';
+$mail= new PHPMailer;
+$mail->Host='smtp.gmail.com'; // SMTP HOST
+$mail->Port=465;
+$mail->CharSet = 'UTF-8';
+$mail->isSMTP();
+$mail->SMTPAuth=true;
+$mail->SMTPSecure='ssl';
+$mail->Username='__SMTP_USERNAME_OR_YOUR_EMAIL_ADDRESS__';
+$mail->Password='__SMTP_PASSWORD_OR_YOUR_PASSWORD__';
+$mail->setFrom('__SMTP_USERNAME_OR_YOUR_EMAIL_ADDRESS__','NAME');
+$mail->addAddress('bdmehadih@gmail.com'); // Where you want to send the mail.
+$mail->addReplyTo('andrewagendi@gmail.com'); // Where you want to get the reply.
+$mail->isHTML(true);
+$mail->Subject='Email subject';
+$mail->Body='Email body.';
+if(!$mail->send())
+{
+echo "Something went wrong";
+echo $mail->ErrorInfo;
+}
+else
+{
+	echo "Email sent successfully";
+}
+?>
